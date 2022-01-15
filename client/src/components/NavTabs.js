2a030b873e38ba904e39b2f9628f7fd87f80eaf0
HobbZ <zachobba@gmail.com>
2022-01-15 22:07:10 +1000

    Bare bones React app and styling created

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,57 @@
+import React from 'react';
+//import '../App.css';
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav">
+      <li className="navItem">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="navItem">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="navItem">
+        <a
+          href="#projects"
+          onClick={() => handlePageChange('Projects')}
+          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+        >
+          Projects
+        </a>
+      </li>
+      <li className="navItem">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="navItem">
+        {/*When clicked will download resume*/}
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          //className={currentPage === 'Contact' ? 'nav-link active' : 'assets/files/Zac-Hobba-TECHNICAL-RESUME.pdf'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <img className="profileImage" src='../client/public/images/profile-pic.WebP' alt='Zac'/>
+      <h1 className="aboutTitle">Hi, I'm Zac</h1>
+      <h3 className="aboutTitle"> CSS | HTML | JavaScript | MySQL | NoSQL | React | CSS style libraries | NPM modules | Node.js </h3>
+
+      <p>
+        Web developer based in the Blue Mountains. Currently learning React. Interested in procedural generation, dynamic created elements and learning AI/ML and AR/VR technology implementations.
+      </p>
+    </div>
+  );
+}
